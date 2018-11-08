32daab24fc4bdf37c9848e2e7dc9c68cb84c19bf
Oleg Klimakov <o.klimakov@corp.mail.ru>
2018-11-08 13:26:54 +0300

    add styles

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Aux from '../../hoc/Aux/Aux'
+import classes from './Layout.module.css';
 
 const layout = ({children}) => (
   <Aux>
     <div>
       Header
     </div>
-    <main>
+    <main className={classes.Content}>
       {children}
     </main>
   </Aux>
