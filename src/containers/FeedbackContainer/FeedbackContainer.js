8785055c99c2813774f57798c1636642aee44dd0
Oleg <klimakovoleg@yandex.ru>
2018-11-18 19:26:15 +0300

    dynamic create input elements

--- src/containers/FeedbackContainer/FeedbackContainer.js
+++ src/containers/FeedbackContainer/FeedbackContainer.js
@@ -2,12 +2,50 @@ import React, { Component } from 'react';
 import Input from '../../components/Input/Input';
 
 class FeedbackContainer extends Component {
+    state = {
+        feedbackForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Ваше имя'
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Ваша почта'
+                },
+                value: ''
+            },
+            phone: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Ваш телефон'
+                },
+                value: ''
+            }
+        }
+    };
+
+
     render () {
+        const formElements = {...this.state.feedbackForm};
+        const inputs = Object.keys(this.state.feedbackForm).map(key => {
+            const inputElement = formElements[key];
+            return <Input
+                key={key}
+                elementType={inputElement.elementType}
+                elementConfig={inputElement.elementConfig}
+                value={inputElement.value}
+            />
+        });
         return (
             <form>
-                <Input inputtype='input' type="text" name="name" placeholder="Ваше имя"/>
-                <Input inputtype='input' type="email" name="email" placeholder="Почта"/>
-                <Input inputtype='input' type="text" name="phone" placeholder="Телефон"/>
+                {inputs}
             </form>
         );
     }
