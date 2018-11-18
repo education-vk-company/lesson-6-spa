92a0a4f2a13d5836848cbb52f5147999824c614a
Oleg <klimakovoleg@yandex.ru>
2018-11-18 19:01:21 +0300

    fix react 16 error

--- src/containers/FeedbackContainer/FeedbackContainer.js
+++ src/containers/FeedbackContainer/FeedbackContainer.js
@@ -5,9 +5,9 @@ class FeedbackContainer extends Component {
     render () {
         return (
             <form>
-                <Input inputType='input' type="text" name="name" placeholder="Ваше имя"/>
-                <Input inputType='input' type="email" name="email" placeholder="Почта"/>
-                <Input inputType='input' type="text" name="phone" placeholder="Телефон"/>
+                <Input inputtype='input' type="text" name="name" placeholder="Ваше имя"/>
+                <Input inputtype='input' type="email" name="email" placeholder="Почта"/>
+                <Input inputtype='input' type="text" name="phone" placeholder="Телефон"/>
             </form>
         );
     }
