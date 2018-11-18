18d1ffd284083e923919004909fd83c7c84f7b4f
Oleg <klimakovoleg@yandex.ru>
2018-11-18 23:19:36 +0300

    simple interceptor, handle request

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,4 +1,23 @@
 import * as actionTypes from './actionTypes';
+import axios from 'axios';
+
+axios.interceptors.response.use((response) => {
+    return response;
+}, (error) => {
+    if (JSON.parse(error.config.data).password.length < 6) {
+       return error
+    } else {
+        return Promise.resolve({
+            data: {
+                token: 123,
+            },
+            status: 200,
+            statusText: 'OK',
+            headers: {},
+            config: error.config
+        });
+    }
+});
 
 export const authStart = () => {
     return {
@@ -23,5 +42,13 @@ export const authFailed = (error) => {
 export const auth = (email, password) => {
     return dispatch => {
         dispatch(authStart());
+        axios.post('login', {email, password})
+            .then(data => {
+                console.log(data);
+                dispatch(authSuccess(data));
+            }).catch(err => {
+                console.log(err);
+                dispatch(authFailed(err))
+            });
     }
 };
